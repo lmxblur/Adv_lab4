c0d50ac01e3904af540228e30a7014fd1b365347
lmxblur <lmxgt5@gmail.com>
2019-09-25 17:11:03 +0200

    Test Class Function

---
+++ lab4/R/class.R
@@ -0,0 +1,27 @@
+linreg <- function(formula,data){
+  linreg1 <- setRefClass("linreg", 
+                         fields =list(formula = 'formula',
+                                      data = 'data.frame'
+                         ),
+                         methods = list(
+                           initialize = function(formula,data){
+                             X <<- model.matrix(formula)
+                             y <<- all.vars(formula)
+                             beta <<- solve(t(X)%*%X)%*%t(X)%*%y
+                             y_fit <<- X%*%beta
+                             e <<- y-X%*%beta
+                             df <<- length(X)-length(beta)
+                             sigma <<- (t(e)%*%e)/df
+                             var <<- sigma%*%solve(t(X)%*%X)
+                             t <<- beta/sqrt(var)},
+                           print = function(){},
+                           plot = function(){},
+                           resid = function(){},
+                           pred = function(){},
+                           coef = function(){},
+                           summary = function(){}
+                         )
+                         
+  )
+  return(linreg1)
+}
